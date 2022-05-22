9f28b8506612f4c15f0a61e003c212c5dcafee00
Doug Russell <doug@getitdownonpaper.com>
2022-05-22 18:40:23 -0400

    Setup project

---
+++ README.md
@@ -0,0 +1,7 @@
+# SpeakUp
+
+Research screen reader for macOS built on [ScreenReader](https://github.com/rustle/ScreenReader).
+
+## License
+
+SpeakUp is released under an Apache license. See the LICENSE file for more information
---
+++ SpeakUp/AppDelegate.swift
@@ -0,0 +1,58 @@
+//
+//  AppDelegate.swift
+//
+//  Copyright © 2017-2022 Doug Russell. All rights reserved.
+//
+
+@preconcurrency import Cocoa
+import AX
+import ScreenReader
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    private lazy var screenReader: ScreenReader = {
+        ScreenReader(dependencies: makeDependencies())
+    }()
+    private func makeDependencies() -> Dependencies {
+        .init(
+            screenReaderDependenciesFactory: {
+                .init(
+                    isTrusted: { AX.isTrusted(promptIfNeeded:$0) },
+                    runningApplicationsFactory: {
+                        await WorkspaceRunningApplications()
+                    }
+                )
+            },
+            serverProviderDependenciesFactory: {
+                .init(
+                    inclusionListFactory: {
+                        [
+                            //"com.apple.finder",
+                        ]
+                    },
+                    exclusionListFactory: {
+                        [
+                            "com.apple.voiceover",
+                            "com.apple.webkit.databases",
+                            "com.apple.webkit.networking",
+                        ]
+                    }
+                )
+            }
+        )
+    }
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        let screenReader = self.screenReader
+        Task.detached {
+            _ = await MainActor.run {
+                NSApplication.shared.setActivationPolicy(.prohibited)
+            }
+            do {
+                await screenReader.confirmTrust()
+                try await screenReader.start()
+            } catch {
+                exit(1)
+            }
+        }
+    }
+}
